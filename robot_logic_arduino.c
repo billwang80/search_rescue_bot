e3dd27b0b81f1e327af0113221aa79c9ac9503e4
billwang80 <billwang80@users.noreply.github.com>
2022-03-21 14:51:12 -0400

    simple ISRs

--- robot_logic_arduino.c
+++ robot_logic_arduino.c
@@ -23,6 +23,30 @@ int in4 = 8;
 
 // ---------- ISRs ----------
 
+void ISR_countA() {
+  counter_A++;
+}
 
+void ISR_countB() {
+  counter_B++;
+}
+
+// convert centimeter to steps
+int cm_to_steps(float cm) {
+  int res;
+
+  float circumference = (wheel_diameter * 3.1415) / 10;
+  float cm_step = circumference / step_count;
+
+  float float_res = cm / cm_step;
+  res = (int) float_res;
+
+  return res
+}
+
+
+void setup() {
+
+}
 
 
